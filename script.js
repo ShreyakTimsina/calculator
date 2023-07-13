7c109a3b74650e4e92588cfa24cb56b2ce06fd2a
Shreyak Timsina <shreyaktimsina@Shreyaks-MacBook-Pro.local>
2023-07-13 16:53:07 +0545

    number string

--- script.js
+++ script.js
@@ -24,9 +24,36 @@ class Calculator {
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]);
+    const decimalDigits = stringNumber.split('.')[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = '';
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = '';
+    }
   }
 
   chooseOperation(operation) {
@@ -66,8 +93,8 @@ class Calculator {
         return;
     }
     this.currentOperand = computation;
-    this.operation = undefined;
     this.previousOperand = '';
+    this.operation = undefined;
   }
 }
 
